3fda87d34ef656d8d60d719dde3ec174f9c09724
sabinkarki <nibaserc@gmail.com>
2016-02-01 11:22:15 -0600

    SpringMVCView

---
+++ src/main/java/com/test/controller/HomeController.java
@@ -0,0 +1,105 @@
+package com.test.controller;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
+
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.test.mydomain.Article;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate);
+		return "home";
+	}
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public LinkedList<Article> get(Model model) {
+		FileReader reader;
+		
+		//FileSystemResource resource;
+		LinkedList<Article> articles = null;
+		try {
+			//resource = new FileSystemResource("classpath:articles.xml");
+			reader = new FileReader(getClass().getClassLoader().getResource("articles.xml").getFile());
+			// convert "unmarshal" data from XML "articles.xml" to Java object
+			// LinkedList<Article>
+			articles = (LinkedList<Article>) Unmarshaller.unmarshal(LinkedList.class, reader);
+			/*for (Article article : articles) {
+				System.out.println(article.getTitle());
+			}*/
+			
+			model.addAttribute("articles", articles);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (MarshalException e) {
+			e.printStackTrace();
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+
+		return articles;
+
+	}
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public LinkedList<Article> json(Model model) {
+		FileReader reader;
+		
+		//FileSystemResource resource;
+		LinkedList<Article> articles = null;
+		try {
+			//resource = new FileSystemResource("classpath:articles.xml");
+			reader = new FileReader(getClass().getClassLoader().getResource("articles.xml").getFile());
+			// convert "unmarshal" data from XML "articles.xml" to Java object
+			// LinkedList<Article>
+			articles = (LinkedList<Article>) Unmarshaller.unmarshal(LinkedList.class, reader);
+			/*for (Article article : articles) {
+				System.out.println(article.getTitle());
+			}*/
+			
+			model.addAttribute("articles", articles);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (MarshalException e) {
+			e.printStackTrace();
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+
+		return articles;
+
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
+<html>
+<head>
+<title>Home</title>
+</head>
+<body>
+	<h1>Spring MVC - View (JSON, XML, PDF, Excel)</h1>
+	<div style="margin: 10px; width: 700px; text-align: center">
+		<a href="json.json">JSON</a> <a
+			href="get.xml" >XML</a> <a
+			href="get.pdf" >PDF</a> <a
+			href="get.xlsx">Excel</a>
+	</div>
+</body>
+</html>
